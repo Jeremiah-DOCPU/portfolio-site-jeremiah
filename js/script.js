69456e3f53da5884fb694176787f233851fc6562
Jeremiah <jeremiahdomo@gmail.com>
2026-03-17 23:17:14 +0000

    Added meaningful page structure and consistent navigation across all pages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// The JavaScript will be added later
+/*
+    Version 2 JavaScript file
+
+    At this stage, JavaScript is not yet being used
+    for any major features.
+
+    It is linked so we can later add:
+
+    - mobile navigation
+    - contact form features
+    - animations
+*/
+
+console.log("Version 2 has loaded successfully.");
